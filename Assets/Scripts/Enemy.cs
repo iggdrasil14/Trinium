fcc056cfe74f5ac49cfeb8feae08efa836aec06d
iggdrasil14 <iggdrasil14@gmail.com>
2024-06-19 21:34:51 +0700

    19.06.2024

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,27 +4,47 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public int health;
-    public float attackDistance;
-    protected Animator animator;
-    protected Rigidbody2D rb;
-    
+    public Vector3 point1;                              // Точка, одна из двух, между которыми двигается враг.
+    public Vector3 point2;                              // Точка, одна из двух, между которыми двигается враг.
+    public Vector3 target;                              // Цель (точка) к которой движется враг.
+    public int health;                                  // Здоровье врага.
+    public float speed;                                 // Скорость перемещения врага.
+    public float attackDistance;                        // Дистанция атаки между врагом и Игроком.
+    private SpriteRenderer enemySR;                     // Компонент SpriteRenderer врага.
+    protected Animator animator;                        // Аниматор врага.
+    protected Rigidbody2D rb;                           // Компонент Rigidbody врага.
+
+    private void Reset()
+    {
+        point1 = transform.position;
+        point2 = transform.position;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        enemySR = GetComponent<SpriteRenderer>();        // Инициализация SpriteRenderer.
+
+        if (enemySR == null)
+        {
+            Debug.LogError("SpriteRenderer компонент не найден!");
+        }
+
+        target = point2;                                // Установка начальной цели движения врага.
     }
 
     // Update is called once per frame
     void Update()
     {
+        Walk();
+        // Проверка на атаку, если Игрок подошел близко.
         float distance = Vector2.Distance(transform.position, GeometryForm.Player.position);
         if (distance <= attackDistance)
         {
             Attack();
         }
-        
     }
 
     public void Idle()
@@ -34,7 +54,22 @@ public class Enemy : MonoBehaviour
 
     public void Walk()
     {
+        // Перемещение врага к цели.
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
 
+        // Переключение цели, если враг достиг изначальной цели.
+        if (Vector3.Distance(transform.position, target) < 0.1f)
+        {
+            target = target == point1 ? point2 : point1;
+            if (target != point1)
+            {
+                enemySR.flipX = true;
+            }
+            if (target != point2)
+            {
+                enemySR.flipX = false;
+            }
+        }
     }
 
     public void Death()
@@ -51,6 +86,4 @@ public class Enemy : MonoBehaviour
     {
 
     }
-
-
 }
